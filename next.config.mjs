a8522af383d86990bc8219db80d545b24c073cde
S7b0t4 <toni.paun.00@mail.ru>
2024-06-08 23:28:26 +0500

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    API_TOKEN: "7358477783:AAFqhM5DZUWF18keUNoFC0EV6I5PZrlxD50",
-    EMAIL_ADDRESS: "anthony.pan.00@gmail.com",
-    EMAIL_PASSWORD: "kjus delc mspu lcqd"
   },
 };
 
